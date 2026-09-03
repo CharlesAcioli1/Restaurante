016fd6640153c2285914d73d8e6e55474459bf33
CharlesAcioli1 <csilva@marlin.com.br>
2026-09-03 10:21:38 -0300

    Atualização dos arquivos em branco

---
+++ src/Restaurante.Services/DTOs/Garcom/CriarGarcomDto.cs
@@ -0,0 +1,9 @@
+namespace Restaurante.Services.DTOs
+{
+    public class CriarGarcomDto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string? Telefone {  get; set; } = string.Empty;
+    }
+}

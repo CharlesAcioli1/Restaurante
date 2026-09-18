88fc658ea98b126e1515f051cece198f7734aa2a
CharlesAcioli1 <csilva@marlin.com.br>
2026-09-18 01:29:02 -0300

    Atualização Controllers

--- src/Restaurante.Presentation/Controllers/GarcomController.cs
+++ src/Restaurante.Presentation/Controllers/GarcomController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Restaurante.Services.DTOs;
+using Restaurante.Services.DTOs.Garcom;
+using Restaurante.Services.Interfaces;
 
 namespace Restaurante.Presentation.Controllers
 {
@@ -7,5 +10,48 @@ namespace Restaurante.Presentation.Controllers
     [ApiController]
     public class GarcomController : ControllerBase
     {
+        private readonly IGarcomService _garcomService;
+
+        public GarcomController(IGarcomService garcomService)
+        {
+            _garcomService = garcomService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTodosAsync()
+        {
+            var garcons = await _garcomService.ObterTodosAsync();
+            return Ok(garcons);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObterPorIdAsync([FromRoute]int id)
+        {
+            var garcons = await _garcomService.ObterPorIdAsync(id);
+            return Ok(garcons);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarGarcomAsync([FromBody] CriarGarcomDto dto)
+        {
+            var garcons = await _garcomService.CriarAsync(dto);
+            return CreatedAtAction(nameof(CriarGarcomAsync), new {id = garcons.Dados}, garcons);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarAsync([FromRoute] int id, [FromBody] AtualizarGarcomDto dto)
+        {
+            var atualizarDtoGarcom = dto with { Id = id };
+            await _garcomService.AtualizarAsync(atualizarDtoGarcom);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarAsync([FromRoute]int id)
+        {
+            await _garcomService.DeletarAsync(id);
+            return NoContent();
+        }
     }
 }
--- src/Restaurante.Presentation/Controllers/MesaController.cs
+++ src/Restaurante.Presentation/Controllers/MesaController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Restaurante.Services.DTOs.Mesa;
+using Restaurante.Services.Interfaces;
 
 namespace Restaurante.Presentation.Controllers
 {
@@ -7,5 +9,46 @@ namespace Restaurante.Presentation.Controllers
     [ApiController]
     public class MesaController : ControllerBase
     {
+        private readonly IMesaService _mesaService;
+
+        public MesaController(IMesaService mesaService)
+        {
+            _mesaService = mesaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTodosAsync()
+        {
+            var mesas = await _mesaService.ObterTodosAsync();
+            return Ok(mesas);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObterPorIdAsyc([FromRoute] int id)
+        {
+            var mesas = await _mesaService.ObterPorIdAsync(id);
+            return Ok(mesas);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CriarAsync([FromBody] CriarMesaDto dto)
+        {
+            var mesa = await _mesaService.CriarAsync(dto);
+            return CreatedAtAction(nameof(CriarAsync), new { Id = mesa.Dados }, mesa);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarAsync([FromRoute] int id, [FromBody] AtualizarMesaDto dto)
+        {
+            var atualizarDtoMesa = dto with { Id = id };
+            await _mesaService.AtualizarAsync(atualizarDtoMesa);
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarAsync(int id)
+        {
+            await _mesaService.DeletarAsync(id);
+            return NoContent();
+        }
     }
 }

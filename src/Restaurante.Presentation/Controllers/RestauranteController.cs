734753f93bffa34b9212e373f73c76d1ed123774
CharlesAcioli1 <csilva@marlin.com.br>
2026-09-18 09:36:53 -0300

    Atualização Controllers

--- src/Restaurante.Presentation/Controllers/RestauranteController.cs
+++ src/Restaurante.Presentation/Controllers/RestauranteController.cs
@@ -8,5 +8,46 @@ namespace Restaurante.Presentation.Controllers
     [ApiController]
     public class RestauranteController : ControllerBase
     {
+        private readonly IRestauranteService _restauranteService;
+
+        public RestauranteController(IRestauranteService restauranteService)
+        {
+            _restauranteService = restauranteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTodosAsync()
+        {
+            var restaurantes =  await _restauranteService.ObterTodosAsync();
+            return Ok(restaurantes);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObterPorIdAsync([FromRoute]int id)
+        {
+            var restaurantes = await _restauranteService.ObterPorIdAsync(id);
+            return Ok(restaurantes);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarAsync([FromBody] CriarRestauranteDto dto)
+        {
+            var restaurantes = await _restauranteService.CriarAsync(dto);
+            return CreatedAtAction(nameof(CriarAsync), new { Id = restaurantes.Dados }, restaurantes);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarAsync([FromRoute] int id, [FromBody] AtualizarRestauranteDto dto)
+        {
+            var atualizarDtoRestaurante = dto with { Id = id };
+            await _restauranteService.AtualizarAsync(atualizarDtoRestaurante);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarAsync(int id)
+        {
+            await _restauranteService.DeletarAsync(id);
+            return NoContent();
+        }
     }
 }
